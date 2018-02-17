5be47b15ed04fd382b2ca38e63807a5e37c2f830
Lucas Cantelli <lucas.cantelli84@gmail.com>
2018-02-17 20:23:13 -0200

    more challenges

---
+++ Algorithms/Divisible-sum-pairs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+internal class Solution
+{
+    private static int divisibleSumPairs(int n, int k, int[] ar)
+    {
+        var possibilities = 0;
+        
+        for (int i = 0; i < ar.Length; i++)
+        {
+            for (int j = 0; j < ar.Length; j++)
+            {
+                if (i < j && (ar[i] + ar[j]) % k == 0)
+                    possibilities++;
+            } 
+           
+        }
+
+        return possibilities;
+
+    }
+
+    private static void Main(string[] args)
+    {
+        var tokens_n = Console.ReadLine().Split(' ');
+        var n = Convert.ToInt32(tokens_n[0]);
+        var k = Convert.ToInt32(tokens_n[1]);
+        var ar_temp = Console.ReadLine().Split(' ');
+        var ar = Array.ConvertAll(ar_temp, int.Parse);
+        var result = divisibleSumPairs(n, k, ar);
+        Console.WriteLine(result);
+    }
+}
---
+++ Algorithms/Migratory-birds/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+class Solution {
+
+    static int MigratoryBirds(int[] ar)
+    {
+        return ar.GroupBy(x => x).OrderByDescending(x => x.Count()).ThenBy(x=>x.Key).First().Key;
+    }
+
+    static void Main() {
+        Console.ReadLine();
+        string[] arTemp = Console.ReadLine()?.Split(' ');
+        int[] ar = Array.ConvertAll(arTemp,Int32.Parse);
+        int result = MigratoryBirds(ar);
+        Console.WriteLine(result);
+    }
+}

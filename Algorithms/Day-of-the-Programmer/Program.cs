673477938374d1826325a510ab3a3b82ec6a90bf
Lucas Cantelli <lucas.cantelli84@gmail.com>
2018-02-19 11:58:08 -0300

    day of the programmer

---
+++ Algorithms/Day-of-the-Programmer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Solution {
+
+    static string Solve(int year)
+    {
+        return DateTime.IsLeapYear(year) ? 
+            new DateTime(year, 1, 1).AddDays(256 - 1).ToString("dd.MM.yyyy") : 
+            new DateTime(year, 1, 1).AddDays(256 - 2).ToString("dd.MM.yyyy");
+    }
+
+    static void Main() {
+        int year = Convert.ToInt32(Console.ReadLine());
+        string result = Solve(year);
+            
+        Console.WriteLine(result);
+    }
+}

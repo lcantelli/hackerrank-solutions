b8ca4d789585af8c5f61b9e4006de028bc4e59e7
Lucas Cantelli <lucas.cantelli84@gmail.com>
2018-02-20 17:07:49 -0300

    Bon Appetit

---
+++ Algorithms/Bon-appetit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Solution
+{
+    internal static class Solution
+    {
+        private static void Main()
+        {
+            var nk = Console.ReadLine();
+            var k = Convert.ToInt32(nk?.Split(' ')[1]);
+
+            var b = Console.ReadLine()
+                ?.Split(' ')
+                .ToList()
+                .Select(x => Convert.ToInt32(x))
+                .ToList();
+
+            var bill = Convert.ToInt32(Console.ReadLine());
+
+            b?.RemoveAt(k);
+
+            var share = (b ?? throw new Exception()).Sum(x => x) / 2;
+
+            Console.WriteLine(share == bill ? "Bon Appetit" : (bill - share).ToString());
+        }
+    }
+}

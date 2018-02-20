68d96b563a65652ddfe418adafd347f2f7c54dd5
Lucas Cantelli <lucas.cantelli84@gmail.com>
2018-02-20 18:47:56 -0300

    Sock-Merchant

---
+++ Algorithms/Sock-Merchant/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Solution
+{
+    private static int SockMerchant(IEnumerable<int> ar)
+    {
+        return ar.GroupBy(a => a).Sum(a => a.Count() / 2);
+    }
+
+    private static void Main()
+    {
+        Console.ReadLine();
+        var arTemp = Console.ReadLine()?.Split(' ');
+        var ar = Array.ConvertAll(arTemp, int.Parse);
+        var result = SockMerchant(ar);
+        Console.WriteLine(result);
+    }
+}

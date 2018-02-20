9f9c1719da9923661c76db209675e1a7c7401277
Lucas Cantelli <lucas.cantelli84@gmail.com>
2018-02-20 19:31:39 -0300

    Clean up

---
+++ Algorithms/Counting-Valleys/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+internal static class Solution {
+    private static int CountingValleys( string s)
+    {
+        var sealevel = 0;
+
+        var hills = 0;
+        
+        foreach (var steps in s.ToCharArray())
+        {
+            bool downMarker;
+            
+            if (steps == 'U')
+            {
+                sealevel++;
+                downMarker = false;
+            }
+            else
+            {
+                sealevel--;
+                downMarker = true;
+            }
+
+            if (sealevel == 0 && !downMarker)
+                hills++;
+        }
+
+        return hills;
+    }
+
+    private static void Main() {
+        Console.ReadLine();
+        var s = Console.ReadLine();
+        var result = CountingValleys(s);
+        Console.WriteLine(result);
+    }
+}
---
+++ Algorithms/Drawing-Book/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+internal static class Solution {
+    private static int Solve(int size, int page)
+    {    
+        var page1 = Math.Abs(page / 2);
+        
+        if(size%2==0){
+            size++;
+        }
+        
+        var page2 = Math.Abs((page - size) / 2);
+        
+        return page1 < page2 ? page1 : page2;
+        
+    }
+
+    private static void Main() {
+        var n = Convert.ToInt32(Console.ReadLine());
+        var p = Convert.ToInt32(Console.ReadLine());
+        var result = Solve(n,p);
+        Console.WriteLine(result);
+    }
+}
